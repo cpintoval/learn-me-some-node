cab1bc77010e03b67683ddc97752a6c2dfa25d56
cpintoval <cpintovalderrama@gmail.com>
2015-10-05 23:50:38 +0000

    11. HTTP File Server

---
+++ 11_http_file_server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (request, response) {
+  fs.createReadStream(process.argv[3]).pipe(response);
+});
+
+server.listen(Number(process.argv[2]));
